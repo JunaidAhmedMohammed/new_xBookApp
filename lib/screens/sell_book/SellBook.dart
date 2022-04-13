454677555837570fd210e8d96bc1992bfeffdd43
moizahmed97 <moizahmed97@gmail.com>
2022-04-13 12:54:23 -0300

    style Sell Book Page

--- lib/screens/sell_book/SellBook.dart
+++ lib/screens/sell_book/SellBook.dart
@@ -10,19 +10,17 @@ import 'package:speech_to_text/speech_to_text.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class SellBook extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Sell Book';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        // resizeToAvoidBottomInset: false,   //new line
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: MyCustomForm(),
+    const appTitle = 'Add Book Details';
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,   //new line
+      appBar: AppBar(
+        backgroundColor: Colors.indigo[400],
+        elevation: 0.0,
+        title: const Text(appTitle, style: TextStyle(fontFamily: "ProductSans"),),
       ),
+      body: MyCustomForm(),
     );
   }
 }
@@ -35,7 +33,6 @@ class MyCustomForm extends StatefulWidget {
 }
 
 class _MyCustomFormState extends State<MyCustomForm> {
-
   // firebase instance
   CollectionReference books = FirebaseFirestore.instance.collection('books');
 
@@ -104,38 +101,38 @@ class _MyCustomFormState extends State<MyCustomForm> {
   void _onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
       _lastWords = result.recognizedWords;
-      print("STT: $_lastWords");
     });
   }
 
   // this function validates data when Submit button is pressed
   // also shows toasts if error found
-  bool validate_data(){
+  bool validate_data() {
     // check if both title and price are empty
-    if(title.isEmpty && price <= 0){
+    if (title.isEmpty && price <= 0) {
       Fluttertoast.showToast(
-        msg: "Book title and price cannot be empty",  // message
+        msg: "Book title and price cannot be empty", // message
         toastLength: Toast.LENGTH_SHORT, // length
-        gravity: ToastGravity.BOTTOM,    // location
+        gravity: ToastGravity.BOTTOM, // location
       );
       return false;
     }
     // check if the title field is empty
-    if(title.isEmpty){
+    if (title.isEmpty) {
       Fluttertoast.showToast(
-          msg: "Book title cannot be empty",  // message
-          toastLength: Toast.LENGTH_SHORT, // length
-          gravity: ToastGravity.BOTTOM,    // location
+        msg: "Book title cannot be empty", // message
+        toastLength: Toast.LENGTH_SHORT, // length
+        gravity: ToastGravity.BOTTOM, // location
       );
       return false;
     }
     // check price
     // show error if price is not numeric or price is <= 0
-    if(!isNumeric(myController_price.text) || double.parse(myController_price.text)<=0){
+    if (!isNumeric(myController_price.text) ||
+        double.parse(myController_price.text) <= 0) {
       Fluttertoast.showToast(
-        msg: "Enter a valid price",  // message
+        msg: "Enter a valid price", // message
         toastLength: Toast.LENGTH_SHORT, // length
-        gravity: ToastGravity.BOTTOM,    // location
+        gravity: ToastGravity.BOTTOM, // location
       );
       return false;
     }
@@ -154,203 +151,203 @@ class _MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      // children: SingleChildScrollView(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: Text(
-            // If listening is active show the recognized words
-            _speechToText.isListening
-                ? 'Last recognized words: $_lastWords'
-            // If lastwords is not empty then show the last word,
-            // otherwise, show instruction of using STT
-                : _lastWords.isNotEmpty? 'Last recognized words: $_lastWords'
-                : 'Instruction: Tap the microphone to start \nlistening & paste in any Text Field',
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // children: SingleChildScrollView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: Text(
+              // If listening is active show the recognized words
+              _speechToText.isListening
+                  ? 'Last recognized words: $_lastWords'
+                  // If lastwords is not empty then show the last word,
+                  // otherwise, show instruction of using STT
+                  : _lastWords.isNotEmpty
+                      ? 'Last recognized words: $_lastWords'
+                      : 'Instruction: Tap the microphone to start \nlistening & paste in any Text Field', style: const TextStyle(fontFamily: "ProductSans"),
+            ),
           ),
-        ),
-        Ink(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          decoration: ShapeDecoration(
-            color: Colors.blue,
-            shape: CircleBorder(),
+          Ink(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            // ignore: prefer_const_constructors
+            decoration: ShapeDecoration(
+              color: Colors.indigo,
+              shape: const CircleBorder(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                  _speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+              color: Colors.white,
+              onPressed:
+                  // If not yet listening for speech start, otherwise stop
+                  _speechToText.isNotListening
+                      ? _startListening
+                      : _stopListening,
+              tooltip: 'Listen',
+            ),
           ),
-          child: IconButton(
-            icon: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
-            color: Colors.white,
-            onPressed:
-            // If not yet listening for speech start, otherwise stop
-            _speechToText.isNotListening ? _startListening : _stopListening,
-            tooltip: 'Listen',
-          ),
-        ),
-        // IconButton(
-        //   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-        //   color: Colors.green,
-        //   icon: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
-        //   onPressed:
-        //   // If not yet listening for speech start, otherwise stop
-        //   _speechToText.isNotListening ? _startListening : _stopListening,
-        //   tooltip: 'Listen',
-        // ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: myController_title,
-            onChanged: (value){
-              title = value;
-              // Call setState to update the UI
-              setState(() {});
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter Book Title (REQUIRED)',
-
-
-              suffixIcon: IconButton(
-                icon: Icon(Icons.paste),
-                onPressed: (){
-                  String value = _lastWords.toString();
-                  myController_title.text = value;
-                  title = value;
-                }
+          // IconButton(
+          //   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          //   color: Colors.green,
+          //   icon: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+          //   onPressed:
+          //   // If not yet listening for speech start, otherwise stop
+          //   _speechToText.isNotListening ? _startListening : _stopListening,
+          //   tooltip: 'Listen',
+          // ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: myController_title,
+              onChanged: (value) {
+                title = value;
+                // Call setState to update the UI
+                setState(() {});
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Book Title',
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: () {
+                      String value = _lastWords.toString();
+                      myController_title.text = value;
+                      title = value;
+                    }),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: myController_author,
-            onChanged: (value){
-              author = value;
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter Author name',
-
-              suffixIcon: IconButton(
-                  icon: Icon(Icons.paste),
-                  onPressed: (){
-                    String value = _lastWords.toString();
-                    myController_author.text = value;
-                    author = value;
-                  }
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: myController_author,
+              onChanged: (value) {
+                author = value;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Author name',
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: () {
+                      String value = _lastWords.toString();
+                      myController_author.text = value;
+                      author = value;
+                    }),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: myController_edition,
-            onChanged: (value){
-              edition = value;
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter Edition',
-
-              suffixIcon: IconButton(
-                  icon: Icon(Icons.paste),
-                  onPressed: (){
-                    String value = _lastWords.toString();
-                    myController_edition.text = value;
-                    edition = value;
-                  }
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: myController_edition,
+              onChanged: (value) {
+                edition = value;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Edition',
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: () {
+                      String value = _lastWords.toString();
+                      myController_edition.text = value;
+                      edition = value;
+                    }),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: myController_price,
-            onChanged: (value){
-              price = double.parse(value);
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter Price (REQUIRED)',
-
-              suffixIcon: IconButton(
-                  icon: Icon(Icons.paste),
-                  onPressed: (){
-                    String value = _lastWords.toString();
-                    myController_price.text = value;
-                    price = double.parse(value);
-                  }
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: myController_price,
+              onChanged: (value) {
+                price = double.parse(value);
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Price',
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: () {
+                      String value = _lastWords.toString();
+                      myController_price.text = value;
+                      price = double.parse(value);
+                    }),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: myController_contact,
-            onChanged: (value){
-              contact = value;
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter Contact Info',
-
-              suffixIcon: IconButton(
-                  icon: Icon(Icons.paste),
-                  onPressed: (){
-                    String value = _lastWords.toString();
-                    myController_contact.text = value;
-                    contact = value;
-                  }
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextField(
+              controller: myController_contact,
+              onChanged: (value) {
+                contact = value;
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Contact Info',
+                suffixIcon: IconButton(
+                    icon: Icon(Icons.paste),
+                    onPressed: () {
+                      String value = _lastWords.toString();
+                      myController_contact.text = value;
+                      contact = value;
+                    }),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: ElevatedButton(
-            child: Text('Submit'),
-            onPressed: () async {
-              // validate data and upload information if firestore database
-              if(validate_data()){
-                await books.add({
-                'uid': uid,
-                'title': title,
-                'author': author,
-                'edition': edition,
-                'price': price,
-                'contact': contact
-                }).then((value) => Fluttertoast.showToast( // show toast if the book is added
-                    msg: "Book added!",  // message
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: ElevatedButton(
+               style: ElevatedButton.styleFrom(
+                    primary: Colors.indigo, // Background color
+                  ),
+              child: const Text('Submit'),
+              onPressed: () async {
+                // validate data and upload information if firestore database
+                if (validate_data()) {
+                  await books
+                      .add({
+                        'uid': uid,
+                        'title': title,
+                        'author': author,
+                        'edition': edition,
+                        'price': price,
+                        'contact': contact
+                      })
+                      .then((value) => Fluttertoast.showToast(
+                            // show toast if the book is added
+                            msg: "Book added!", // message
+                            toastLength: Toast.LENGTH_SHORT, // length
+                            gravity: ToastGravity.BOTTOM, // location
+                          ))
+                      .catchError((error) => Fluttertoast.showToast(
+                            // show error if failed to add
+                            msg: "Failed to add book info: $error", // message
+                            toastLength: Toast.LENGTH_SHORT, // length
+                            gravity: ToastGravity.BOTTOM, // location
+                          ));
+
+                  // show toast
+                  Fluttertoast.showToast(
+                    msg: "Book title cannot be empty", // message
                     toastLength: Toast.LENGTH_SHORT, // length
-                    gravity: ToastGravity.BOTTOM,    // location
-                  ))
-                    .catchError((error) => Fluttertoast.showToast( // show error if failed to add
-                      msg: "Failed to add book info: $error",  // message
-                      toastLength: Toast.LENGTH_SHORT, // length
-                      gravity: ToastGravity.BOTTOM,    // location
-                    ));
+                    gravity: ToastGravity.BOTTOM, // location
+                  );
 
-                // show toast
-                Fluttertoast.showToast(
-                  msg: "Book title cannot be empty",  // message
-                  toastLength: Toast.LENGTH_SHORT, // length
-                  gravity: ToastGravity.BOTTOM,    // location
-                );
-
-                _navigateToHomeScreen(context);
-              }
-            },
+                  _navigateToHomeScreen(context);
+                }
+              },
+            ),
           ),
-        ),
-      ],
-      // ),
-
-    ),
+        ],
+        // ),
+      ),
     );
   }
+
   void _navigateToHomeScreen(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Home()));
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Home()));
   }
 }
-
